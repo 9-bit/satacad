0e23952b4ca4ba64d5dc6268be60bed2ccc2ad04
Saturday Academy <satacad@apple1.local>
2013-04-27 11:00:30 -0700

    first deploy

--- app.rb
+++ app.rb
@@ -1,8 +1,25 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
+
+
+
 get '/' do
   erb :index   
+end
+
+get '/login' do
+  @username = params [:username]
+  @password = params [:password]
+  if @username == 'jpoz'
+    if @password == 'taco'
+      "well done sir, Welcome"
+    else
+      "Come on dude, what's the right password?"
+    end
+  else
+    "You are not JPOZ"
+  end
 end
 
 get '/name' do
